92e58ef8f2a3e6dbe7104c29f42556e75c005a99
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-22 18:07:41 +0300

    description component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import Description from "./components/description/Description";
 function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+  return <div className="App">
+    <Description />
+  </div>;
 }
 
 export default App;
---
+++ src/components/description/Description.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Description.css";
+function Description() {
+  return (
+    <div className="desc-wrapper">
+      <h2 className="heading">Join our community</h2>
+      <h3 className="money-back">30-day,hassle-free money back guarantee</h3>
+      <p className="desc-text">
+        Gain access to our full library of tutorials along with expert code
+        reviews. Perfect for any developers who are serious about honing their
+        skills.
+      </p>
+    </div>
+  );
+}
+
+export default Description;
