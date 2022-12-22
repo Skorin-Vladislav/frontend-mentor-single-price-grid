5b835b6977c85044261e18ccffc50ab4e509c091
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-22 18:59:43 +0300

    pros component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Subscription from "./components/subscription/Subscription";
 import Description from "./components/description/Description";
+import Pros from "./components/pros/Pros";
 function App() {
   return (
     <div className="App">
       <Description />
       <Subscription />
+      <Pros />
     </div>
   );
 }
---
+++ src/components/pros/Pros.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Pros.css";
+function Pros() {
+  return (
+    <div className="pros-wrapper">
+      <h2>Why Us</h2>
+      <ul>
+        <li>Tutorials by industry experts</li>
+        <li>Peer & expert code review</li>
+        <li>Coding exercises</li>
+        <li>Access to our GitHub repos</li>
+        <li>Community forum</li>
+        <li>Flashcard decks</li>
+        <li>New videos every week</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Pros;
