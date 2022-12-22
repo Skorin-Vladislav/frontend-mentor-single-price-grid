e175d518f3cdc4110d40c712872de771750762ff
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2022-12-22 18:42:18 +0300

    subscription and other components changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Subscription from "./components/subscription/Subscription";
+import Description from "./components/description/Description";
 function App() {
   return (
     <div className="App">
+      <Description />
       <Subscription />
     </div>
   );
